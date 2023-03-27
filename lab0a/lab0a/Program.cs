817c1b51516a01d1c46398857dabddac271c6298
pond1111 <127219087+pond1111@users.noreply.github.com>
2023-03-27 23:03:02 +0700

    lab0a

---
+++ lab0a/lab0a/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Commanline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name: Aphithun Duangchunkloi");
+            Console.WriteLine("Student Number: 643450092-1");
+            Console.WriteLine("Email: Aphithun.d@kkumail.com");
+            Console.ReadKey();
+        }
+    }
+}
